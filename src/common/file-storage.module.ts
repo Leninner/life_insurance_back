8a2feff30c8b184b5f34426bd7d9d9623e70c461
leninner <mazabandalenin180@gmail.com>
2025-07-01 18:36:28 -0500

    feat: add prediction project

--- src/common/file-storage.module.ts
+++ src/common/file-storage.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { FileStorageService } from './services/file-storage.service'
 import { S3StorageStrategy } from './services/s3-storage.strategy'
-import { LocalStorageStrategy } from './services/local-storage.strategy'
 
 @Module({
   imports: [ConfigModule],
-  providers: [FileStorageService, S3StorageStrategy, LocalStorageStrategy],
-  exports: [FileStorageService, S3StorageStrategy, LocalStorageStrategy],
+  providers: [FileStorageService, S3StorageStrategy],
+  exports: [FileStorageService, S3StorageStrategy],
 })
 export class FileStorageModule {}
